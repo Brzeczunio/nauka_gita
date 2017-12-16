45aa256f13d485c2fe7f6f4d02a1492208441c7a
Brzeczunio <xxx@xx.pl>
2017-12-16 11:52:18 +0100

    Nowy program z ćwiczeń

---
+++ tuple.py
@@ -0,0 +1,70 @@
+tab = [1,2,3,4,5][::2]
+
+print(tab)
+
+li = [1,2,3,4,5]
+
+print(li)
+li[1] = 12
+
+print(li)
+
+
+
+a= ('a', 'b', 'c')
+
+print(a)
+
+s = 'asdasd'
+
+print(s)
+
+s = set(s)
+
+print(s)
+
+
+a=[x for x in range(1000) if x%10==0]
+
+print(a)
+
+def g():
+    for i in range(1000):
+        yield i
+
+
+a = g()
+
+print(a.next())
+
+print(a.next())
+
+print(a.next())
+
+
+a = [1,2,3,4,5]
+
+import copy
+
+b = copy.copy(a)
+
+print(a,b)
+
+a[3] = 221
+
+print(a,b)
+
+i = range(1,11)
+
+for i in i:
+    print(i)
+else:
+    print('koniec')
+
+
+for x in range(11,18):
+    print('lec dalej')
+    continue
+
+
+print 12
